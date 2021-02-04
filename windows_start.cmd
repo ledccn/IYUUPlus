f3f41dcf291fa95f1493cf5872d55ee9ede03f1e
david <nz1985@qq.com>
2021-02-04 18:45:53 +0800

    fix

--- windows_start.cmd
+++ windows_start.cmd
@@ -57,12 +57,19 @@ git --version
 git fetch --all
 git reset --hard origin/master
 echo 升级完成！
+echo.
 goto :checkPHP
 
 :checkPHP
-if exist "%~dp0php/php.exe" (set PHP_BINARY=%~dp0php\php.exe) else (set PHP_BINARY=php.exe)
-goto :start
-
+if exist "%~dp0php\php.exe" (set PHP_BINARY=%~dp0php\php.exe) else (set PHP_BINARY=php.exe)
+echo PHP二进制程序：%PHP_BINARY%
+%PHP_BINARY% -v|find "PHP Group">nul&&goto :start
+cls
+echo 没有检测到PHP执行程序，脚本运行终止！！！
+echo 如果您已下载过php程序，请在解压缩之后，把php文件夹添加进系统的环境变量。
+echo 或者把php执行程序，解压缩到当前目录下的‘php’文件夹。
+pause
+goto :end
 
 :start
 rem 运行脚本
