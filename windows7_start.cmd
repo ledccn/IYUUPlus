3e425206d9f98f78a4c111deecedd23b18e645ca
david <nz1985@qq.com>
2021-09-09 17:37:54 +0800

    修改：移除wenda

--- windows7_start.cmd
+++ windows7_start.cmd
@@ -6,7 +6,6 @@ echo Docker: https://hub.docker.com/r/iyuucn/iyuuplus
 echo Gitee: https://gitee.com/ledc/iyuuplus
 echo Github: https://github.com/ledccn/IYUUPlus
 echo QQ: 859882209 / 931954050 / 924099912
-echo Wenda: http://wenda.iyuu.cn
 echo Token: https://iyuu.cn
 echo Blog: https://www.iyuu.cn
 echo Docs: http://api.iyuu.cn/docs.php
--- windows_start.cmd
+++ windows_start.cmd
@@ -1,4 +1,4 @@
-@echo off
+﻿@echo off
 chcp 65001
 title IYUUPlus
 cd /d "%~dp0"
@@ -6,7 +6,6 @@ echo Docker: https://hub.docker.com/r/iyuucn/iyuuplus
 echo Gitee: https://gitee.com/ledc/iyuuplus
 echo Github: https://github.com/ledccn/IYUUPlus
 echo QQ: 859882209 / 931954050 / 924099912
-echo Wenda: http://wenda.iyuu.cn
 echo Token: https://iyuu.cn
 echo Blog: https://www.iyuu.cn
 echo Docs: http://api.iyuu.cn/docs.php
@@ -34,7 +33,6 @@ cd IYUUPlus
 goto :checkPHP
 
 :installError
-
 echo "当前运行环境未检测到GIT程序，源码安装失败！请手动下载"
 echo "Docker: https://hub.docker.com/r/iyuucn/iyuuplus"
 echo "Gitee: https://gitee.com/ledc/iyuuplus"
